6dcbf07a9019d9e516bd3032633424881a75c5bf
Pablosudev <pablo.losu.dev@gmail.com>
2025-02-10 09:02:22 +0100

    hover

--- popup.js
+++ popup.js
@@ -1,50 +1,55 @@
-const popUp = document.getElementById("popUp")
-const buttonClose = document.getElementById("buttonClose")
-const buttonEmail = document.getElementById("buttonEmail")
-const input = document.getElementById("inputPopUp")
-//Preparamos la función que nos abrirá el popUp a los 5s.
-setTimeout(() => {
-    popUp.showModal();
-},300);
-//Damos funcionalidad a nuestro botón.
+const popUp = document.getElementById("popUp");
+const buttonClose = document.getElementById("buttonClose");
+const buttonEmail = document.getElementById("buttonEmail");
+const input = document.getElementById("inputPopUp");
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const openPopUp = setTimeout(() => {
+  popUp.showModal();
+}, 5000);
+
+
 buttonClose.addEventListener("click", () => {
-    popUp.close();
-})
-//Guardamos los datos del email en nuestra API al hacer click en el botón de enviar.
-buttonEmail.addEventListener("click", () => {
   popUp.close();
-    function checkInputEmail(event) {
-        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-        return regex.test(input);
-    }
+});
 
-    if(!regex.test(input)){
-        event.preventDefault();
-        alert("Introduce un correo valido")
-    }
+buttonEmail.addEventListener("click", () => {
+  event.preventDefault();
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(input)
+  if (!regex.test(input)) {
+    alert("Introduce un correo valido");
+    return;
+  }
+
+  popUp.close();
+
+  fetch("https://jsonnplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: input.value }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error: " + response.status);
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-          throw new Error('Error: ' + response.status);
-        }
-        return response.json(); // Parseamos la respuesta como JSON
-      })
-      .then(responseData => {
-        console.log('Datos recibidos:', responseData);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-    
-      
+    .then((responseData) => {
+      console.log("Datos recibidos:", responseData);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
 
-    
-})
-   
+window.addEventListener("click", (event) => {
+  if (
+    popUp.open &&
+    !popUp.contains(event.target) &&
+    event.target !== buttonClose
+  ) {
+    popUp.close(); // Cerramos el pop-up
+  }
+});
